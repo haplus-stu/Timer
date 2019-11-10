3de5ac0ed529cf67d23830679284327ace779bf3
haplus-stu <57579123+haplus-stu@users.noreply.github.com>
2019-11-10 19:23:48 +0900

    Update index.js
    var→let
    function作成

--- index.js
+++ index.js
@@ -1,29 +1,45 @@
 'use strict';
 
-var work_time = 0; 
-var break_time = 300;//test sec 2 true sec 300
-var nowStatus = 0;
-var timer;
-var break_count;
-var audio_elm = new Audio();
+let work_time = 0; 
+let break_time = 300;//test sec 2 true sec 300
+let now_status = 0;
+let timer;
+let break_count;
+let audio_elm = new Audio();
+
+
+function getTimer() {
+  return document.getElementById("timer");
+}
+
+function getStart() {
+  return document.getElementById("start");
+}
+
+function getBreak() {
+  return   document.getElementById("break");
+}
+
+function getReset() {
+  return document.getElementById("reset");
+}
 
 function cntStart() {
-  document.getElementById("reset").disabled = false;
-  document.getElementById("start").disabled = true;
+  getReset().disabled = false;
+  getStart().disabled = true;
   work_time = 1500;//test sec 5 //true sec 1500
-  //document.getElementById("break").disabled= true;
-  document.getElementById("timer").innerText =  work_time; 
+  getTimer().innerText =  work_time; 
   timer = setInterval("countDown()",1000);
 }
 
 function countDown() {
-  nowStatus = 1;
+  now_status = 1;
   audio_elm.src = "Clock-Alarm03-01(Loop).mp3"
 
   work_time --;
-  document.getElementById("timer").innerText =  work_time; 
+  getTimer().innerText =  work_time; 
   if(work_time<=0){
-    document.getElementById("break").disabled= false;
+    getBreak().disabled= false;
     reSet();
     audio_elm.play();
     audio_elm.loop();
@@ -39,8 +55,8 @@ function reSet() {
 
 function move_Break() {
   break_time=300;
-  document.getElementById("timer").innerText = break_time;
-  document.getElementById("start").disabled=true;
+  getTimer().innerText = break_time;
+  getStart().disabled=true;
   audio_elm.pause();
   audio_elm.currentTime = 0;
   break_count = setInterval("Breaktime()",1000);
@@ -49,15 +65,15 @@ function move_Break() {
 
 function break_reSet() {
   clearInterval(break_count);
-  document.getElementById("start").disabled= false;
+  getStart().disabled= false;
   console.log("succsess!");
 }
 
 function Breaktime() {
-  nowStatus = 0;
+  now_status = 0;
   audio_elm.src = "Clock-Alarm03-01(Loop).mp3"
   break_time --;
-  document.getElementById("timer").innerText = break_time;
+ getTimer().innerText = break_time;
   if(break_time <= 0){
     break_reSet();
     audio_elm.play();
@@ -65,22 +81,24 @@ function Breaktime() {
 }
 
   function force_reSet() {
-    if(nowStatus==1){
+    if(now_status==1){
       clearInterval(timer);
-      document.getElementById("timer").innerText= work_time=0;
-      document.getElementById("start").disabled = false;
-      document.getElementById("break").disabled = true;
-      document.getElementById("reset").disabled = true;
+      getTimer().innerText= work_time=0;
+      getStart().disabled = false;
+      getBreak().disabled = true;
+      getReset().disabled = true;
       audio_elm.pause();
       audio_elm.currentTime = 0;
       }
     
-      if(nowStatus== 0){
+      if(now_status== 0){
         clearInterval(break_count);
-        document.getElementById("timer").innerText= break_time=0;
-        document.getElementById("break").disabled = true;
-        document.getElementById("start").disabled = false;
+        getTimer().innerText= break_time=0;
+        getBreak().disabled = true;
+        getStart().disabled = false;
         audio_elm.pause();
         audio_elm.currentTime = 0;
       }
+
+
   }
