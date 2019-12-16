dad3ee21bb6dd83b3384e09b2730af1b8fed9f01
Hasu <syu8.0324@outlook.jp>
2019-12-16 22:51:43 +0900

    fixed bug

--- index.js
+++ index.js
@@ -38,20 +38,24 @@ function need_timer_value(i) {
 
 function Start_count() {
 
+  if(now_status=0){
+      get_Reset().disabled = false;
+      get_Start().disabled = false;
+  }else if(now_status=1){
+      get_Reset().disabled = false;
+      get_Start().disabled = true;
+  }
+
   min = parseInt(need_timer_value(0));
   sec = parseInt(need_timer_value(1));
-  
-
-  get_Reset().disabled = false;
-  get_Start().disabled = true;
-
+ 
   work_time = (min*60)+sec;
 
   let msg = zeroPadding(min,2)+":"+zeroPadding(sec,2);
 
   remaing_Time().innerHTML = msg;
 
-  timer = setInterval('countDown()', 1000);
+  timer = setInterval('countDown()', 1000); 
 }
 
 
@@ -73,6 +77,8 @@ function countDown() {
 
   if (work_time <= 0) {
     clearInterval(timer);
+    now_status = 0;
+    get_Start().disabled = false;
     get_Break().disabled = false;
     audio_elm.play();
   }
@@ -80,25 +86,30 @@ function countDown() {
 
 
 function move_Break() {
-  
+
    break_min = parseInt(need_timer_value(2));
    break_sec = parseInt(need_timer_value(3));
 
-   break_time = break_min*60+break_sec;
+   break_time = (break_min*60)+break_sec;
     remaing_Time().innerText = zeroPadding(break_min,2)+":"+zeroPadding(break_sec,2);
-    get_Start().disabled = true;
     audio_elm.pause();
     audio_elm.currentTime = 0;
-    break_count = setInterval('Breaktime()', 1000);
+
+   if(now_status==1){
+      get_Start().disabled = false;
+      get_Break().disabled = false;  
+    }else if(now_status==0){
+      get_Start().disabled = true;
+      get_Break().disabled = true;
+    }
+
+    timer = setInterval('Breaktime()', 1000);
 }
 
-function break_reSet() {
-  clearInterval(break_count);
-  get_Start().disabled = false;
-}
 
 function Breaktime() {
-  now_status = 0;
+
+  now_status = 1;
 
   audio_elm.src = 'Clock-Alarm03-01(Loop).mp3';
 
@@ -107,12 +118,13 @@ function Breaktime() {
   }else{
     break_time--;
   }  
+
   let count_break_min = Math.floor(break_time/60);  
   let count_break_sec = Math.floor(break_time%60);
 
   remaing_Time().innerText = zeroPadding(count_break_min,2)+":"+zeroPadding(count_break_sec,2);
   if (break_time <= 0) {
-    break_reSet();
+    clearInterval(timer);
     audio_elm.play();
   }
 }
@@ -128,7 +140,7 @@ function force_reSet() {
   }
 
   if (now_status == 0) {
-    clearInterval(break_count);
+    clearInterval(timer);
     remaing_Time().innerText = break_time = 0;
     get_Break().disabled = true;
     get_Start().disabled = false;
