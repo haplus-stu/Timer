e1d0e4e932df28044ed3e384672cfe0692a34ef8
Vagrant Default User <syu8.0324@outlook.jp>
2020-04-09 15:37:45 +0900

    コンフリクト修正

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 'use strict';
 
-let min,sec,timer;
+let min, sec, timer;
 let break_count;
 let key_name;
 let now_status = 0;
-let i ; 
+let i;
 let cnt = 0;
 let audio_elm = new Audio();
-let work_time,break_time;
-let break_min,break_sec;
-let pelm = document.getElementById('preset');			
+let work_time, break_time;
+let break_min, break_sec;
+let pelm = document.getElementById('preset');
 
 
 function get_Start() {
@@ -24,8 +24,8 @@ function get_Reset() {
   return document.getElementById('reset');
 }
 
-function zeroPadding(num,len){
-  return (Array(len).join('0')+num).slice(-len);
+function zeroPadding(num, len) {
+  return (Array(len).join('0') + num).slice(-len);
 }
 
 function remaing_Time() {
@@ -33,40 +33,39 @@ function remaing_Time() {
 }
 
 
-function view_preset(){
+function view_preset() {
   let ul = document.querySelector('.preset_wrapper');
-  if(ul.classList.contains('open')){
+  if (ul.classList.contains('open')) {
     ul.classList.remove('open');
-    document.getElementById('disp_pattern').innerText='プリセット一覧'
-  }else{
+    document.getElementById('disp_pattern').innerText = 'プリセット一覧'
+  } else {
     ul.classList.add('open');
-    document.getElementById('disp_pattern').innerText='閉じる'
-  }  
+    document.getElementById('disp_pattern').innerText = '閉じる'
+  }
 }
 
 
 //プリセット表示関数
-//プリセット名表示まで完了
-window.onload = function(){
+window.onload = function () {
 
   let select_preset = document.querySelectorAll('.preset');
   console.log(select_preset);
-  
 
-	if(localStorage.length > 0){
 
-    for(let j=0; j<select_preset.length; j++) select_preset[j].classList.add('isActive');
-    document.getElementById('save_pattern').textContent='作成';
+  if (localStorage.length > 0) {
 
-		for(i=0;i < localStorage.length; i++){
+    for (let j = 0; j < select_preset.length; j++) select_preset[j].classList.add('isActive');
+    document.getElementById('save_pattern').textContent = '作成';
+
+    for (i = 0; i < localStorage.length; i++) {
       key_name = localStorage.key(i);
-			const liEl = document.createElement('option');
+      const liEl = document.createElement('option');
       liEl.textContent = key_name;
-      liEl.setAttribute('value',i);
+      liEl.setAttribute('value', i);
       pelm.appendChild(liEl);
       console.log(liEl);
-		}
-	}else if(localStorage.length == 0){
+    }
+  } else if (localStorage.length == 0) {
     const nothting_msg = document.createElement('p');
     nothting_msg.textContent = '保存されたプリセットはありません。';
     const preset_wrapper = document.querySelector('.preset_wrapper');
@@ -77,7 +76,7 @@ window.onload = function(){
 
 //時間の要素取得が多くなったので引数で指定できる関数化
 function need_timer_value(i) {
-  if(document.timer.elements[i].value == "") {
+  if (document.timer.elements[i].value == "") {
     return document.timer.elements[i].value = 0;
   }
   return document.timer.elements[i].value;
@@ -85,63 +84,63 @@ function need_timer_value(i) {
 
 
 
-function save_pattern(){
-  let preset_name = window.prompt('プリセット名を入力してください','無題のプリセット');
-  if(preset_name == null) throw alert('キャンセルされました。');
-  
-  let w_min = window.prompt('集中したい分数を入力してください','0');
-  if(w_min  == null) throw alert('キャンセルされました。');
+function save_pattern() {
+  let preset_name = window.prompt('プリセット名を入力してください', '無題のプリセット');
+  if (preset_name == null) throw alert('キャンセルされました。');
 
-  let w_sec = window.prompt('集中したい秒数を入力してください','0');
-  if(w_sec  == null) throw alert('キャンセルされました。');
-  
-  let b_min = window.prompt('休憩したい分数を入力してください','0');
-  if(b_min  == null) throw alert('キャンセルされました。');
-  
-  let b_sec = window.prompt('休憩したい秒数を入力してください','0');
-  if(b_sec  == null) throw alert('キャンセルされました。');
+  let w_min = window.prompt('集中したい分数を入力してください', '0');
+  if (w_min == null) throw alert('キャンセルされました。');
+
+  let w_sec = window.prompt('集中したい秒数を入力してください', '0');
+  if (w_sec == null) throw alert('キャンセルされました。');
+
+  let b_min = window.prompt('休憩したい分数を入力してください', '0');
+  if (b_min == null) throw alert('キャンセルされました。');
+
+  let b_sec = window.prompt('休憩したい秒数を入力してください', '0');
+  if (b_sec == null) throw alert('キャンセルされました。');
 
 
-  if(w_min == 0 && w_sec == 0 && b_min == 0 && b_sec == 0){
-    throw  alert('すべての入力値が0です');
+  if (w_min == 0 && w_sec == 0 && b_min == 0 && b_sec == 0) {
+    throw alert('すべての入力値が0です');
   }
 
 
-  let time_pattern= {
-    work_min: parseInt(w_min)*60,
+  let time_pattern = {
+    work_min: parseInt(w_min) * 60,
     work_sec: parseInt(w_sec),
-    break_min: parseInt(b_min)*60,
+    break_min: parseInt(b_min) * 60,
     break_sec: parseInt(b_sec),
-};
+  };
 
 
-	let obj = JSON.stringify(time_pattern);
+  let obj = JSON.stringify(time_pattern);
 
-	localStorage.setItem(preset_name,obj);
+  localStorage.setItem(preset_name, obj);
   location.reload();
 
   console.log(time_pattern);
-//	console.log(obj_name);
+  //	console.log(obj_name);
 }
 
 
 
-function set_preset_value(btn){
+function set_preset_value(btn) {
   let preset_no = document.getElementById('preset').value;
 
-	key_name = localStorage.key(preset_no);
-	let jsonObj = localStorage.getItem(key_name);
-	let jsObj = JSON.parse(jsonObj);
+  key_name = localStorage.key(preset_no);
+  let jsonObj = localStorage.getItem(key_name);
+  let jsObj = JSON.parse(jsonObj);
 
 
-	document.timer.elements[0].value = jsObj.work_min/60;
-	document.timer.elements[1].value = jsObj.work_sec;
-	document.timer.elements[2].value = jsObj.break_min/60;
-	document.timer.elements[3].value = jsObj.break_sec;
+  document.timer.elements[0].value = jsObj.work_min / 60;
+  document.timer.elements[1].value = jsObj.work_sec;
+  document.timer.elements[2].value = jsObj.break_min / 60;
+  document.timer.elements[3].value = jsObj.break_sec;
 }
 
-function remove_preset(btn){
-	key_name = localStorage.key(btn.value);
+function remove_preset(btn) {
+  key_name = localStorage.key(btn.value);
   localStorage.removeItem(key_name);
   location.reload();
 }
@@ -150,24 +149,24 @@ function remove_preset(btn){
 
 function Start_count() {
 
-  if(now_status=0){
-      get_Reset().disabled = false;
-      get_Start().disabled = false;
-  }else if(now_status=1){
-      get_Reset().disabled = false;
-      get_Start().disabled = true;
+  if (now_status = 0) {
+    get_Reset().disabled = false;
+    get_Start().disabled = false;
+  } else if (now_status = 1) {
+    get_Reset().disabled = false;
+    get_Start().disabled = true;
   }
 
   min = parseInt(need_timer_value(0));
   sec = parseInt(need_timer_value(1));
- 
-  work_time = (min*60)+sec;
 
-  let msg = zeroPadding(min,2)+":"+zeroPadding(sec,2);
+  work_time = (min * 60) + sec;
+
+  let msg = zeroPadding(min, 2) + ":" + zeroPadding(sec, 2);
 
   remaing_Time().innerHTML = msg;
 
-  timer = setInterval('countDown()', 1000); 
+  timer = setInterval('countDown()', 1000);
 }
 
 
@@ -175,16 +174,16 @@ function countDown() {
   now_status = 1;
   audio_elm.src = 'Clock-Alarm03-01(Loop).mp3';
 
-  if(work_time<=0){
-      work_time = 0;
-  }else{
+  if (work_time <= 0) {
+    work_time = 0;
+  } else {
     work_time--;
   }
 
-  min = Math.floor(work_time/60); 
-  sec = Math.floor(work_time%60);
-  
-  remaing_Time().innerHTML = zeroPadding(min,2)+":"+zeroPadding(sec,2);
+  min = Math.floor(work_time / 60);
+  sec = Math.floor(work_time % 60);
+
+  remaing_Time().innerHTML = zeroPadding(min, 2) + ":" + zeroPadding(sec, 2);
 
 
   if (work_time <= 0) {
@@ -199,23 +198,23 @@ function countDown() {
 
 function move_Break() {
 
-   break_min = parseInt(need_timer_value(2));
-   break_sec = parseInt(need_timer_value(3));
+  break_min = parseInt(need_timer_value(2));
+  break_sec = parseInt(need_timer_value(3));
 
-   break_time = (break_min*60)+break_sec;
-    remaing_Time().innerText = zeroPadding(break_min,2)+":"+zeroPadding(break_sec,2);
-    audio_elm.pause();
-    audio_elm.currentTime = 0;
+  break_time = (break_min * 60) + break_sec;
+  remaing_Time().innerText = zeroPadding(break_min, 2) + ":" + zeroPadding(break_sec, 2);
+  audio_elm.pause();
+  audio_elm.currentTime = 0;
 
-   if(now_status==1){
-      get_Start().disabled = false;
-      get_Break().disabled = false;  
-    }else if(now_status==0){
-      get_Start().disabled = true;
-      get_Break().disabled = true;
-    }
+  if (now_status == 1) {
+    get_Start().disabled = false;
+    get_Break().disabled = false;
+  } else if (now_status == 0) {
+    get_Start().disabled = true;
+    get_Break().disabled = true;
+  }
 
-    timer = setInterval('Breaktime()', 1000);
+  timer = setInterval('Breaktime()', 1000);
 }
 
 
@@ -225,19 +224,21 @@ function Breaktime() {
 
   audio_elm.src = 'Clock-Alarm03-01(Loop).mp3';
 
-  if(break_time<=0){
-    break_time = 0;  
-  }else{
+  if (break_time <= 0) {
+    break_time = 0;
+  } else {
     break_time--;
-  }  
+  }
 
-  let count_break_min = Math.floor(break_time/60);  
-  let count_break_sec = Math.floor(break_time%60);
+  let count_break_min = Math.floor(break_time / 60);
+  let count_break_sec = Math.floor(break_time % 60);
 
-  remaing_Time().innerText = zeroPadding(count_break_min,2)+":"+zeroPadding(count_break_sec,2);
+  remaing_Time().innerText = zeroPadding(count_break_min, 2) + ":" + zeroPadding(count_break_sec, 2);
   if (break_time <= 0) {
     clearInterval(timer);
     audio_elm.play();
+    get_Start().disabled = false;
+    get_Break().disabled = get_Reset().disabled = true;
   }
 }
 
@@ -253,11 +254,10 @@ function force_reSet() {
 
   if (now_status == 0) {
     clearInterval(timer);
-	remaing_Time().innerText = break_time ="00:00";
+    remaing_Time().innerText = break_time = "00:00";
     get_Break().disabled = true;
     get_Start().disabled = false;
     audio_elm.pause();
     audio_elm.currentTime = 0;
   }
 }
-
